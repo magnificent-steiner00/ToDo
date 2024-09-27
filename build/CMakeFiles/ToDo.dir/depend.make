08231d9d8fe6d8ed2da8e261276edcba4ea5d53a
Your Name <youremail@example.com>
2024-09-27 21:55:00 +0530

    database displaying

---
+++ build/CMakeFiles/ToDo.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ToDo.
+# This may be replaced when dependencies are built.
---
+++ build/CMakeFiles/ToDo_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ToDo_autogen"
+  "ToDo_autogen/include/ui/ui_mainwindow.h"
+  "ToDo_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ToDo_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
